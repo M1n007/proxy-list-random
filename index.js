e68889cea840e0cabc02ea202203d99a62cbe933
Amin <amin4udin@gmail.com>
2019-03-10 00:19:56 +0700

    v1.0.1

---
+++ index.js
@@ -0,0 +1,29 @@
+const fetch = require("node-fetch");
+const util = require("util");
+
+module.exports = () =>
+  new Promise((resolve, reject) => {
+    fetch("http://spys.me/proxy.txt")
+      .then(res => res.text())
+      .then(ipport => {
+        const regex = /[0-9]+(?:\.[0-9]+){3}:[0-9]+/gm;
+        const allIP = [];
+
+        while ((m = regex.exec(ipport)) !== null) {
+          if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+          }
+
+          m.map(ip => {
+            allIP.push(ip);
+          });
+        }
+        console.log(util.isArray(allIP));
+        resolve(
+          util.inspect(allIP, {
+            maxArrayLength: null,
+            colors: true
+          })
+        );
+      });
+  });
